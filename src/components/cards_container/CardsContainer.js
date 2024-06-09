c63bf0d18c434ddb16722b35c5a0e2127d2d354b
Marie Faleeva <134315162+Laydena@users.noreply.github.com>
2024-06-09 14:33:22 +0300

    add useState

--- src/components/cards_container/CardsContainer.js
+++ src/components/cards_container/CardsContainer.js
@@ -1,36 +1,67 @@
 import Card from '../card/Card.js';
 import CardActive from '../cardActive/CardActive.js';
 import data from '../../Data.json';
-import style from './cardscontainer.module.css'
+import style from './cardscontainer.module.css';
+import React, { useState } from 'react';
 
 export default function CardsContainer() {
+    const [active, setActive] = useState(false);
+    const handleActive = (event) => {
+        setActive(!active);
+    }
+
     return (
         <div className={style.wrapper}>
-            {data.map((item, index) => (
-                item.boolean
-                    ?
-                    < CardActive
-                        name={item.name}
-                        price={item.price}
-                        speed={item.speed}
-                        description={item.description}
-                        color={item.color}
-                        key={index}
-                    />
-                    :
-                    < Card
-                        name={item.name}
-                        price={item.price}
-                        speed={item.speed}
-                        description={item.description}
-                        color={item.color}
-                        key={index}
-                    />
+            {data.map((item, index) => {
+                return (
+                    active
+                        ?
+                        <button className={style.btn} onClick={handleActive} key={index}>
+                            < CardActive
+                                name={item.name}
+                                price={item.price}
+                                description={item.description}
+                                speed={item.speed}
+                                color={item.color}
+                            />
+                        </button>
+                        :
+                        <button className={style.btn} onClick={handleActive} key={index}>
+                            < Card
+                                name={item.name}
+                                price={item.price}
+                                description={item.description}
+                                speed={item.speed}
+                                color={item.color}
+                            />
+                        </button>
 
-            ))
-
-            }
+                )
+            })}
 
         </div>
+
+
+        // <div className={style.wrapper}>
+        //     {data.map((item, index) => (
+        //         active
+        //             ?
+        //             <button className={style.btn} onclick={handleActive} >
+        //                 < CardActive
+        //                     key={index} {...item}
+        //                 />
+        //             </button>
+        //             :
+        //             <button className={style.btn} onclick={handleActive}>
+        //                 < Card
+        //                     key={index} {...item}
+        //                 />
+        //             </button>
+
+        //     ))
+
+        //     }
+
+        // </div>
     )
 }
