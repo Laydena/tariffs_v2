49564de5bdfd43dac44fa46f095b370a33109838
Marie Faleeva <134315162+Laydena@users.noreply.github.com>
2024-06-15 17:38:30 +0300

    add index

--- src/components/cards_container/CardsContainer.js
+++ src/components/cards_container/CardsContainer.js
@@ -5,36 +5,37 @@ import style from './cardscontainer.module.css';
 import React, { useState } from 'react';
 
 export default function CardsContainer() {
-    const [active, setActive] = useState(false);
-    const handleActive = (event) => {
-        setActive(!active);
-    }
+    const [activeIndex, setActiveIndex] = useState(null);
+    const handleActive = (index) => {
+        setActiveIndex(index === activeIndex ? null : index);
+    };
 
     return (
         <div className={style.wrapper}>
             {data.map((item, index) => {
+                const isActive = index === activeIndex;
                 return (
-                    active
-                        ?
-                        <button className={style.btn} onClick={handleActive} key={index}>
+                    <button className={style.btn} onClick={() => handleActive(index)} key={index}>
+                        {isActive ? (
                             < CardActive
                                 name={item.name}
                                 price={item.price}
                                 description={item.description}
                                 speed={item.speed}
                                 color={item.color}
-                            />
-                        </button>
-                        :
-                        <button className={style.btn} onClick={handleActive} key={index}>
-                            < Card
-                                name={item.name}
-                                price={item.price}
-                                description={item.description}
-                                speed={item.speed}
-                                color={item.color}
-                            />
-                        </button>
+                            />)
+                            : (
+                                < Card
+                                    name={item.name}
+                                    price={item.price}
+                                    description={item.description}
+                                    speed={item.speed}
+                                    color={item.color}
+                                />
+                            )}
+
+
+                    </button>
 
                 )
             })}
